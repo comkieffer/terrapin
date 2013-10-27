8cd938f67ffb0e058be374db990cbba79a5222ca
Thibaud Chupin <thibaud.chupin@gmail.com>
2013-10-27 22:54:02 +0100

    Better warnings and fixed intelligent mode

--- programs/turtle/digMine.lua
+++ programs/turtle/digMine.lua
@@ -1,6 +1,8 @@
 
 local lapp = require "pl.lapp"
 local List = require "pl.List"
+local pretty = require "pl.pretty"
+local tablex = require "pl.tablex"
 
 local ui = require "ui"
 local terrapin = require "terrapin"
@@ -34,13 +36,13 @@ function digMine(cmdLine)
 		terrapin.dig()
 		terrapin.digDown(0)
 
+		if cmdLine.intelligent_mining then
+			terrapin.explore(cmdLine.trash_blocks)
+		end
+		
 		if place_torches and (i % 10 == 0) then
 			-- makeAlcove returns false when we have run out of torches
 			place_torches = makeAlcove(cmdLine.torch_slots)
-		end
-
-		if cmdLine.intelligent_mining then
-			terrapin.explore(cmdLine.trash_blocks)
 		end
 
 		steps = steps + 1
@@ -77,19 +79,21 @@ end
 
 local args = { ... }
 local usage = [[
-Dig a series of mineshafts. Recommended setup is torhes, ender chest and intelligent mining enabled.
+Dig a series of mineshafts. Recommended setup is torches, ender chest and intelligent mining enabled.
 For a complete description of the options see the documentation.
 
-<mines> (default 1)
--d, --direction (default right)
--n, --no-torches 
--l, --length (default 100)
--s, --spacing (default 3)
--e, --ender-chest use an enderchest to dump mined inventory
--i, --intelligent-mining
+<mines> (default 1)          How many mines to dig
+-d, --direction (default r)  Where to turn to start the next mine
+-n, --no-torches             Do not lay torches
+-l, --length (default 100)   What length should the mine be
+-s, --spacing (default 3)    How far apart should 2 mines be
+-e, --ender-chest            Use an enderchest to dump mined inventory
+-i, --intelligent-mining     Dump materials into ender chest when overflowing
 ]]
-
 local cmdLine = lapp(usage, args)
+
+local p = require "pl.pretty"
+p.dump(cmdLine)
 
 --check fuel level
 local required_moves = cmdLine.length * 2 + 2
@@ -98,6 +102,9 @@ if not ui.confirmFuel(required_moves) then
 end
 
 -- set options
+
+-- Torches can be in slots 1 -> 4, To identify torch slots the turtle just looks for full slots 
+-- in that range. Do not put anything but torches in there.
 local torch_slots = List({1, 2, 3, 4}):filter(function(el) 
 		if terrapin.getItemCount(el) == 0 then 
 			return false
@@ -107,34 +114,49 @@ local torch_slots = List({1, 2, 3, 4}):filter(function(el)
 	end
 )
 
-local required_torches = math.floor(cmdLine.length / 10) 
-local total_torches = torch_slots:reduce(function(a, b) return a + b end)
+-- Test whether the turtle has torches in its inventory
+if next(torch_slots) == nil and not(cmdLine.no_torches) then
+	error("Add torches to turtle inventory or specify -n option")
+end
 
-if not(cmdLine.no_torches) and (required_torches < total_torches) then
-	if not ui.confirm("Not enough torches to completely light up the gallery.\n"
-	                  .. required_torches .. " needed, " .. terrapin.getItemCount(1) 
-	                  .. " available.\n\n Continue anyway ?"
-	    ) then
-		return
+
+if not(cmdLine.no_torches) then 
+	local required_torches = math.floor(cmdLine.length / 10) 
+	local total_torches = tablex.reduce(function(a, b) return a + b end, torch_slots)
+
+	if (required_torches < total_torches) then
+		if not ui.confirm("Not enough torches to completely light up the gallery.\n"
+		                  .. required_torches .. " needed, " .. total_torches 
+		                  .. " available.\n\n Continue anyway ?"
+		    ) then
+			return
+		end
 	end
 end
 
 cmdLine.torch_slots = torch_slots
 
-if cmdLine.ender_chest and terrapin.getItemCount(5) ~= 0 then
-	cmdLine.ender_chest_slot = 5
+-- The ender-chest, if present should be store in slot 5
+if cmdLine.ender_chest then
+	if terrapin.getItemCount(5) == 0 then
+		error("The ender chest needs to be in slot 5")
+	else
+		cmdLine.ender_chest_slot = 5
+	end
 end
 
+-- figure out which slots contain the blocks that don't interest us
 if cmdLine.intelligent_mining then
+	print ("making trash blocs")
 	-- get the trash blocks table
 	local trash_blocks = terrapin.getOccupiedSlots()
 	
-	-- remove torch slots
+	-- remove torch slots from junk slots
 	for i = 1, #cmdLine.torch_slots do
 		trash_blocks:pop(cmdLine.torch_slots[i])
 	end
 
-	-- remove ender chest slot
+	-- remove ender chest slot from junk slots
 	if cmdLine.ender_chest_slot then
 		trash_blocks:pop(cmdLine.ender_chest_slot)
 	end
@@ -147,30 +169,34 @@ if cmdLine.intelligent_mining then
 		return false
 	end
 
-	cmdLine.trash_blocks = trash_blocks
+	print(
+		"Intelligent mining enabled. Considering blocks in the follwing slots as "
+		.. "trash : " .. trash_blocks:join(", ")
+	)
+
+	pretty.dump(trash_blocks)
+	cmdLine.trash_blocks = { unpack(trash_blocks) }
 end
 
-if cmdLine.intelligent_mining and not cmdLine.ender_chest and not
-	ui.confirm("When using intelligent mining using ender chest is recommended. Without it your "
-	           .. "turtle might have to return extremely often to the start of the mine rendering "
-	           ..  "progress extremely slow.\n\n Continue anyway ?") then
-	return false
-end
+-- tell terrapin how to match blocks in explore mode :
+-- in this mode the valuable blocks are the ones that don't match the compare
+-- function
+terrapin.search_for_valuable_blocks = false
 
-
+-- start main program
 if cmdLine.mines > 0 then
 	for i = 1, cmdLine.mines do
 		io.write("digging mine " .. 1 .. " of " .. cmdLine.mines .. "\n")
 		terrapin.digUp()
 		digMine(cmdLine)
- 		terrapin.dropAllExcept({1})
+ 		-- terrapin.dropAllExcept({1})
 
 		if i ~= cmdLine.mines then -- on the last mine we don't need to go to the next
-			if cmdLine.direction == "right" then
+			if cmdLine.direction == "r" then
 				terrapin.turnLeft()
 				terrapin.dig(cmdLine.spacing + 1)
 				terrapin.turnLeft()
-			elseif cmdLine.direction == "left" then
+			elseif cmdLine.direction == "l" then
 				terrapin.turnRight()
 				terrapin.dig(cmdLine.spacing + 1)
 				terrapin.turnRight()
