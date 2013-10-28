f6d85043bf34105c84fc2843d2cf5427ab1dc8e4
Thibaud Chupin <thibaud.chupin@gmail.com>
2013-10-28 21:16:14 +0100

    Detect that the specified slot does not contain fuel

--- programs/turtle/refuel.lua
+++ programs/turtle/refuel.lua
@@ -1,25 +1,29 @@
 
+local lapp     = require "pl.lapp"
 local terrapin = require "terrapin"
 
-local args = { ... }
+local args, usage = { ... }, [[
+Refuel the turtle from the inventory
+<fuel-slot> (default 1) What slot is the fuel in
+]]
+local cmdLine = lapp(usage, args)
 
-local fuel_slot = 1
+local initial_fuel_level = terrapin.getFuelLevel()
+io.write("Current fuel level : " .. initial_fuel_level .."\n")
 
-if #args == 1 then
-	fuel_slot = tonumber(args[1])	
-end
-
-io.write("Current fuel level : " .. turtle.getFuelLevel() .."\n")
-
-local fuel_count = terrapin.getItemCount(fuel_slot)
-terrapin.select(fuel_slot)
+local fuel_count = terrapin.getItemCount(cmdLine.fuel_slot)
+terrapin.select(cmdLine.fuel_slot)
 
 if fuel_count == 0 then 
-	io.write("No fuel in slot " .. fuel_slot .. "\n")
+	io.write("No fuel in slot " .. cmdLine.fuel_slot .. "\n")
 	return
 else
 	io.write("pulling " .. fuel_count .. " from slot " .. fuel_slot .. "\n")
 	turtle.refuel(fuel_count)
 end
 
-io.write("Fuel after refueling : " .. turtle.getFuelLevel() .. "\n")  
+if terrapin.getFuelLevel() == initial_fuel_level then
+	error("Item in slot " .. cmdLine.fuel_slot .. " is not a fuel")
+else
+	io.write("Fuel after refueling : " .. turtle.getFuelLevel() .. "\n")  
+end
