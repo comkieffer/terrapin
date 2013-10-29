f1f6f4a389ff6abac2ff53d1012797985676b991
Thibaud Chupin <thibaud.chupin@gmail.com>
2013-10-29 18:21:59 +0100

    Fixed file extensions

--- installer_cfg.lua
+++ installer_cfg.lua
@@ -7,13 +7,13 @@ return {
 			["update always"] = false,
 
 			["files"] = {
-				"app.lua", "array2d.lua", "class.lua", "compat.lua", "comprehension"    ,
-				"config.lua", "data.lua", "Date.lua", "dir.lua", "func.lua", "import_into"  ,
-				"init.lua", "input.lua", "lapp.lua", "lexer.lua", "List.lua", "luabalanced" ,
-				"Map.lua", "MultiMap.lua", "operator.lua", "OrderedMap.lua", "permute"  ,
-				"pretty.lua", "seq.lua", "Set.lua", "sip.lua", "strict.lua", "stringio"     ,
-				"stringx.lua", "tablex.lua", "template.lua", "test.lua", "text"             ,
-				"types.lua", "utils.lua", "xml"                                 ,
+				"app.lua", "array2d.lua", "class.lua", "compat.lua", "comprehension.lua"       ,
+				"config.lua", "data.lua", "Date.lua", "dir.lua", "func.lua", "import_into.lua" ,
+				"init.lua", "input.lua", "lapp.lua", "lexer.lua", "List.lua", "luabalanced.lua",
+				"Map.lua", "MultiMap.lua", "operator.lua", "OrderedMap.lua", "permute.lua"     ,
+				"pretty.lua", "seq.lua", "Set.lua", "sip.lua", "strict.lua", "stringio.lua"    ,
+				"stringx.lua", "tablex.lua", "template.lua", "test.lua", "text.lua"            ,
+				"types.lua", "utils.lua", "xml.lua",
 			}
 		},
 		["common apis"] = {
@@ -22,7 +22,8 @@ return {
 			["update always"] = true,	
 
 			["files"] = {
-				"config.lua", "pickle.lua", "require.lua", "rsx.lua", "termx.lua", "ui.lua", "utils.lua", "vector"   ,
+				"config.lua", "pickle.lua", "require.lua", "rsx.lua", "termx.lua", "ui.lua"   ,
+				"utils.lua", "vector.lua",
 			}
 		},
 		["turtle apis"] = {
@@ -49,7 +50,7 @@ return {
 			["update always"] = true,	
 
 			["files"] = {
-				"clearMountain.lua", "cut.lua.lua",
+				"clearMountain.lua", "cut.lua",
 				"digMine.lua",  "digNext.lua", "digPit.lua", "digStair.lua", "digTunnel.lua", 
 				"refuel.lua", "rc.lua",
 			}
