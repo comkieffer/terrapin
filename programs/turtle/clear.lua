2e5c7cbff391bdf3004dbea2682dee04f9b95ee8
Thibaud Chupin <thibaud.chupin@gmail.com>
2014-09-25 00:39:57 +0200

    Renamed clearMountain to clear

--- programs/turtle/clear.lua
+++ programs/turtle/clear.lua
@@ -1,6 +1,17 @@
 
+local lapp     = require "pl.lapp"
 local ui       = require "ui"
 local terrapin = require "terrapin"
+
+local function inventoryFull()
+	dig_pos = terrapin.getPos()
+
+	terrapin.goToStart()
+	terrapin.dropAll()
+
+	terrapin.goTo(dig_pos)
+end
+
 
 local args = { ... }
 local usage = [[
@@ -10,34 +21,41 @@ local usage = [[
 
 local cmdLine = lapp(usage, args)
 
--- estimate fuel consumption. 
+-- estimate fuel consumption.
 -- we suppose that a "mountain" is on averag 3 blocks high
 local required_moves = (cmdLine.length * 5) * cmdLine.width
 if not ui.confirmFuel(required_moves) then
 	return
 end
 
+terrapin.enableInertialNav()
+
 for i = 1, cmdLine.width do
 	for j = 1, cmdLine.length do
-		terrapin.dig() 
+		terrapin.dig()
 
-		-- dig all blocks above us 
-		local steps = 0		
+		-- dig all blocks above us
+		local steps = 0
 		while terrapin.detectUp() do
 			terrapin.digUp()
 			steps = steps + 1
 		end
 
 		terrapin.down(steps)
+
+		if #terrapin.getFreeSlots() == 0 then
+			inventoryFull()
+		end
+
 	end
 	print(width, cmdLine.length)
 	terrapin.turn(2)
-	terrapin.forward(cmdLine.length)
+	terrapin.dig(cmdLine.length)
 
 	-- position for next mine
 	if i ~= cmdLine.width then
 		terrapin.turnLeft()
-		terrapin.forward()
+		terrapin.dig()
 		terrapin.turnLeft()
 	end
 end
