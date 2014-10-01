f7216dcbc3afa8808d4c9e3e3b39fb3c2c99779b
Thibaud Chupin <thibaud.chupin@gmail.com>
2014-10-01 02:24:42 +0200

    Add checkins to DigPit

--- terrapin/programs/turtle/digPit.lua
+++ terrapin/programs/turtle/digPit.lua
@@ -10,13 +10,16 @@
 local lapp = require "pl.lapp"
 local ui = require "ui"
 local terrapin = require "terrapin"
+local checkin = require "checkin"
 
 local function inventoryFull()
+	checkin.checkin('Invetory Full. Returning to surface.')
 	dig_pos = terrapin.getPos()
 
 	terrapin.goToStart()
 	terrapin.dropAll()
 
+	checkin.checkin('Invetory emptied. Returning to pit.')
 	terrapin.goTo(dig_pos)
 end
 
@@ -28,6 +31,7 @@ local usage = [[
 ]]
 
 local cmdLine = lapp(usage, args)
+checkin.startTask('DigPit', cmdLine)
 
 local required_moves = cmdLine.width * cmdLine.length * (cmdLine.depth + 1)
 if not ui.confirmFuel(required_moves) then
@@ -46,6 +50,8 @@ terrapin.turn(2)
 
 for i = 1, cmdLine.width do
 	for j = 1, cmdLine.length / 2 do
+		checkin.checkin('Digging pit ' .. j .. ' of ' .. cmdLine.length / 2 ..
+			' in slice ' .. i .. ' of ' .. cmdLine.width .. '.')
 
 		-- Dig Down
 		for k = 1, cmdLine.depth do
@@ -59,7 +65,6 @@ for i = 1, cmdLine.width do
 				inventoryFull()
 			end
 		end
-
 
 		-- climb back to the top. We shouldn't mine anything on the way up
 		terrapin.dig()
@@ -86,5 +91,6 @@ for i = 1, cmdLine.width do
 	end
 end
 
+checkin.checkin('Finished digging. Returning to starting point.')
 terrapin.goToStart()
 terrapin.dropAll()
