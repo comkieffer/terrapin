64415d72812581e709f63a9792f6ad8deac1256e
Thibaud Chupin <thibaud.chupin@gmail.com>
2014-10-01 02:23:57 +0200

    Extend rc to read commands from the command line

--- terrapin/programs/turtle/rc.lua
+++ terrapin/programs/turtle/rc.lua
@@ -1,5 +1,7 @@
 
 local terrapin = require "terrapin"
+local args = { ... }
+
 
 keyActionHandlers = {
 	[200] = {"forward", terrapin.dig},
@@ -13,6 +15,32 @@ keyActionHandlers = {
 
 print "waiting for input ... "
 
+-- If we have args we just execute them
+if #args > 1 then
+	if #args == 2 then
+		local actions = {
+			["f"] = terrapin.dig,
+			["b"] = terrapin.back,
+			["r"] = terrapin.turnRight,
+			["l"] = terrapin.turnLeft,
+			["u"] = terrapin.digUp,
+			["d"] = terrapin.digDown,
+		}
+
+		-- If the action is valid exectute it.
+		if actions[ args[1] ] then
+			for i = 0, tonumber(args[2]) do
+				actions[ args[1] ]()
+			end
+		end
+	else
+		error('Malformed Command line')
+	end
+
+	return
+end
+
+-- Looks like the command line is empty. Go to interactive mode ...
 running = true
 while running do
 	eventType, keyCode = os.pullEvent("key")
