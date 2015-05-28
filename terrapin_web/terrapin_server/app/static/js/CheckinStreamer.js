86e772d9d43500118e6c9df6761e84d959b2d177
Thibaud Chupin <thibaud.chupin@gmail.com>
2015-05-28 12:19:51 +0200

    Added the checkin-stream container to the required pages.
    The current implementation is untestsed and probably doesn't work but the frontend should be OK. We still need to implement most of the backend stuff: filtering checkins to only receive the relevant ones, making sure that we only receive checkins from computers we control, ....

--- terrapin_web/terrapin_server/app/static/js/CheckinStreamer.js
+++ terrapin_web/terrapin_server/app/static/js/CheckinStreamer.js
@@ -5,21 +5,49 @@
  */
 
 var CheckinStreamer = {
+	checkin_count: 0,
 
 	start: function(target) {
-		var self = this;
-		self.target = target;
+		this.target = target;
 
 		target.append('<p>Waiting for checkins ...</p>');
-		document.addEventListener('CheckinSource:new', onCheckinReceived);
+		document.addEventListener('CheckinSource:new', this.onCheckinReceived);
 	},
 
 	onCheckinReceived: function(event) {
-		var rendered = self.render(event.detail.data);
+		var rendered = this.renderCheckin(event.detail.data);
+
+		if (this.checkin_count == 0)
+			this.target.clear();
+
+		this.checkin_count += 1;
 		target.append(rendered);
 	},
 
 	renderCheckin: function(checkin) {
-		return 'TODO';
+		function render_tmpl(tmpl, data){
+			for(var p in data)
+				tmpl = tmpl.replace(new RegExp('{'+p+'}','g'), data[p]);
+			return tmpl;
+		}
+
+		var tmpl =
+		'<div class="log-line>'                                                +
+		'	<span class="created-at">{created_at}</span>'                      +
+		'	<span class="computer-name">{computer_id}: {computer_name}</span>' +
+		'	<span class="task-name">{task}</span>'                             +
+		'	<span class="{message_class}">{status</span>';
+
+		var data = {
+			created_at    : moment.unix(checkin.created_at)
+				.format("D MMM YYYY hh:mm"),
+			comuter_id    : checkin.computer_id,
+			computer_name : checkin.computer_name,
+			task          : checkin.task,
+			message_class : (checkin.type == 'error')?
+				"message bg-danger" : "message"
+		}
+
+		return render_tmpl(tmpl, data)
 	},
 };
