e5707a476a9dcd0ae26b0b08d70e7297729588c5
Thibaud Chupin <thibaud.chupin@gmail.com>
2015-06-02 19:58:46 +0200

    Add ckstat program to view checkin status on turtles.

---
+++ terrapin/ac-get/terrapin-core/bin/ckstat.lua
@@ -0,0 +1,39 @@
+
+local checkin = require 'checkin.client'
+local stringx = require 'sanelight.stringx'
+
+local screen =[[
++------------------------------------+
+|  Checkin Status:                   |
+|                                    |
+|    Configuration Status: %s  |
+|    Connection Status:    %s  |
+|    Sent Messages:        %s  |
+|    Failed Messages:      %s  |
+|                                    |
+|    Current Task:         %s  |
+|                                    |
+|              Press Any Key to Exit |
++------------------------------------+]]
+
+
+
+local function busy_wait()
+	local event = os.pullEvent('key')
+end
+
+
+term.clear()
+term.setCursorPos(1, 1)
+
+local status = checkin.status()
+for k,v in pairs(status) do status[k] = tostring(v) end
+
+print(screen:format(
+	stringx.rjust(status['is_configuration_ok'] and 'OK' or 'FAIL', 8),
+	stringx.rjust(status['is_server_available'] and 'OK' or 'FAIL', 8),
+	stringx.rjust(status['sent_messages'], 8),
+	stringx.rjust(status['failed_messages'], 8),
+	stringx.rjust(status['current_task'], 8)
+))
+busy_wait()
