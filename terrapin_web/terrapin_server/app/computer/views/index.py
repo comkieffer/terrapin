debadad84e2f137c5144894ff42048400fdc14ee
Thibaud Chupin <thibaud.chupin@gmail.com>
2015-07-01 17:31:40 +0200

    Remove Unneeded import

--- terrapin_web/terrapin_server/app/computer/views/index.py
+++ terrapin_web/terrapin_server/app/computer/views/index.py
@@ -3,8 +3,6 @@ from flask            import render_template
 from flask.ext.classy import FlaskView, route
 from flask.ext.login  import current_user, login_required
 
-from ..queries        import getWorldsFor
-
 class IndexView(FlaskView):
 	route_base = '/'
 
